60b38f639d395fa6c992a615db58ece98a967d9a
WORK <WORK@WORKPC>
2018-12-05 01:56:06 +0200

    Initial commit

---
+++ BladeFlurryCharges/AutoPhaseRunSettings.cs
@@ -0,0 +1,20 @@
+﻿using PoeHUD.Hud.Settings;
+using PoeHUD.Plugins;
+using System.Windows.Forms;
+namespace AutoPhaseRun
+{
+    public class AutoPhaseRunSettings : SettingsBase
+    {
+        public AutoPhaseRunSettings()
+        {
+            delay = new RangeNode<int>(4, 1, 10);
+            pressedKey = Keys.W;
+        }
+       
+        [Menu("Key", 1)]
+        public HotkeyNode pressedKey { get; set; }
+
+        [Menu("Delay After Pressing Key", 2)]
+        public RangeNode<int> delay { get; set; }
+    }
+}
