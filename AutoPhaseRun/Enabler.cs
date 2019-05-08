75aafb4050c4106f74bd7c1e25edc1b3289ef1f2
Marchosiae <tpiardi@gmail.com>
2019-05-08 10:11:42 -0300

    Try to separate classes
    But still didnt work
    Method error: Render : System.NullReferenceException: Referência de objeto não definida para uma instância de um objeto. em AutoPhaseRun.Enabler.Enabled() em AutoPhaseRun.AutoPhaseRun.Render() em PoeHUD.Plugins.BasePlugin._Render()

---
+++ AutoPhaseRun/Enabler.cs
@@ -0,0 +1,42 @@
+﻿using PoeHUD.Poe.Components;
+using System;
+using PoeHUD.Plugins;
+using System.Windows.Forms;
+using PoeHUD.Controllers;
+
+namespace AutoPhaseRun
+{
+    public class Enabler : BaseSettingsPlugin<AutoPhaseRunSettings>
+    {
+        public int enabled = 1;
+        public int Enabled()
+        { 
+              
+            //Pause if Hideout, city or dead character
+            if
+                (GameController.Area.CurrentArea.IsHideout
+                || GameController.Area.CurrentArea.IsTown
+                || !GameController.Player.IsAlive
+                || !Settings.Enable
+                || GameController.Game.IngameState.Data.LocalPlayer.GetComponent<Life>().Buffs.Exists(b => b.Name == "grace_period")
+                || GameController.Game.IngameState.IngameUi.InventoryPanel.IsVisible
+                || GameController.Game.IngameState.IngameUi.OpenLeftPanel.IsVisible
+                || GameController.Game.IngameState.IngameUi.AtlasPanel.IsVisible
+                || GameController.Game.IngameState.IngameUi.OpenRightPanel.IsVisible
+                || GameController.Game.IngameState.IngameUi.SyndicatePanel.IsVisible
+                || GameController.Game.IngameState.IngameUi.IncursionWindow.IsVisible
+                || GameController.Game.IngameState.IngameUi.TreePanel.IsVisible
+                || GameController.Game.IngameState.IngameUi.HiddenSkillBar.IsVisible
+                || GameController.Game.IngameState.UIRoot.GetChildFromIndices(1, 111).ChildCount > 0
+                || Control.IsKeyLocked(Keys.CapsLock) == false)
+            {
+                enabled = 0;
+                }
+            return enabled;
+
+            
+        }
+    }
+}
+
+
